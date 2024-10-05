7c0ddd847cd811d22d78c28604e9bfe8f963f02d
sarahwonago <sarahshaaleh@gmail.com>
2024-10-05 15:56:54 +0300

    models for category, fooditem, specialoffer, dinningtable

--- cafeadminend/admin.py
+++ cafeadminend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, DiningTable, FoodItem, SpecialOffer
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(DiningTable)
+admin.site.register(FoodItem)
+admin.site.register(SpecialOffer)
