4f71b4daa49ca6bca82bfe66c266897f8ea85c51
sarahwonago <sarahshaaleh@gmail.com>
2024-10-05 16:01:46 +0300

    media settings for development stage

--- tastymealsproject/urls.py
+++ tastymealsproject/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
@@ -22,4 +24,7 @@ urlpatterns = [
     path('api/cafeadmin/', include('cafeadminend.urls')),
     path("api/account/", include('account.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
