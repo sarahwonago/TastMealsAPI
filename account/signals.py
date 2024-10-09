26f5f0d6669119baae08fd37595c4c9c7b77741a
sarahwonago <sarahshaaleh@gmail.com>
2024-10-09 14:46:01 +0300

    signals to create customers loyalty point

--- account/signals.py
+++ account/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 
-from customerend.models import Cart
+from customerend.models import Cart, CustomerLoyaltyPoint
 
 User = get_user_model()
 
@@ -10,7 +10,12 @@ User = get_user_model()
 def create_users_cart(sender, instance, created, **kwargs):
     """
     Automatically creates a new cart when a new user is successfully created.
+    Automatically creates a new customerloyalty point when a new user is successfully created.
     """
 
     if created:
+        # creates users cart
         Cart.objects.create(user=instance)
+
+        # creates customers loyalty point
+        CustomerLoyaltyPoint.objects.create(user=instance)
